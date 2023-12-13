fe7afa15839389b41d2642fb48e0491a7e78499b
SaturnX-Dev <Saturnx-dev@outlook.com>
2023-12-13 17:30:32 -0600

    Se libera la primera version funcional V.1

--- Filter_List.py
+++ Filter_List.py
@@ -1,3 +1,5 @@
+import os
+
 # Función para filtrar User Agents por navegador
 def filter_by_browser(user_agents, browser):
     filtered_agents = [ua for ua in user_agents if browser.lower() in ua.lower()]
@@ -6,55 +8,68 @@ def filter_by_browser(user_agents, browser):
 # Función para filtrar User Agents por sistema operativo
 def filter_by_os(user_agents, os_name):
     if os_name.lower() == 'mac os x':
-        # Filtrar solo las entradas específicas de Mac OS X
         filtered_agents = [ua for ua in user_agents if 'mac os x' in ua.lower() and 'iphone' not in ua.lower() and 'ipad' not in ua.lower()]
+    elif os_name.lower() == 'ios':
+        filtered_agents = [ua for ua in user_agents if 'iphone' in ua.lower() or 'ipad' in ua.lower()]
+    elif os_name.lower() == 'linux':
+        filtered_agents = [ua for ua in user_agents if 'linux' in ua.lower() and 'android' not in ua.lower()]
+    elif os_name.lower() == 'android':
+        filtered_agents = [ua for ua in user_agents if 'android' in ua.lower()]
     else:
-        # Filtrar el resto de las entradas por el nombre del sistema operativo
         filtered_agents = [ua for ua in user_agents if os_name.lower() in ua.lower()]
     
     return filtered_agents
 
+# Función para guardar los resultados en un archivo de texto
+def save_results(results):
+    file_name = input("Ingresa el nombre del archivo para guardar los resultados (sin extensión): ")
+    file_name += ".txt"
+    with open(file_name, 'w') as file:
+        for result in results:
+            file.write(result + '\n')
+    print(f"Resultados guardados en '{file_name}'")
 
 # Función principal
 def main():
-    # Leer el archivo de User Agents
     with open('User Agents List.txt', 'r') as file:
         user_agents = file.read().splitlines()
 
     while True:
         print("""
- _______________________________________________________________________________
-|                                                                               |
-|                            User Agent Filter List                             |
-|                                by Saturnx-Dev                                 |
-|           _________________________________________________________           |
-|                                                                               | 
-|                                |  MAIN MENU |                                 |
-|           _________________________________________________________           |
-|                                                                               |
-|        _______________________________________________________________        |
-|                                                                               |
-|        Filtra una lista de User Agents por Equipo, SO o por Navegador.        |
-|        _______________________________________________________________        |
-|                                                                               |
-|      [1] Filtrar por Navegador      | Filtra los User Agents por navegador    |
-|      [2] Filtrar por SO        | Filtra los User Agents por sistema operativo |
-|      [3] Salir                      | Salir del programa                      |
-|                                                                               |
-|_______________________________________________________________________________|
-    """)
+ ___________________________________________________________________________________
+|                                                                                   |
+|                               User Agent List Filter                              |
+|                                   by Saturnx-Dev                                  |
+|             _________________________________________________________             |
+|                                                                                   | 
+|                                   |  MAIN MENU |                                  |
+|               https://github.com/SaturnX-Dev/User-Agent-List-Filter               |
+|                                  Made with love <3                                |
+|          _______________________________________________________________          |
+|                                                                                   |
+|              Filtra una lista de User Agents por SO o por Navegador.              |
+|          _______________________________________________________________          |
+|                                                                                   |
+|      [1] Filtrar por Navegador     | Filtra los User Agents por navegador         |
+|      [2] Filtrar por SO            | Filtra los User Agents por sistema operativo |
+|      [3] Salir                     | Salir del programa                           |
+|                                                                                   |
+|___________________________________________________________________________________|
+
+        """)
         option = input("Selecciona una opción (1, 2, 3): ")
 
         if option == '1':
-            browser_option = input("""
+            print("""
  _______________________________________________________________________________
 |                                                                               |
-|                                 | BROWSER MENU |                              |
+|                                | BROWSER MENU |                               |
 |           _________________________________________________________           |
 |                                                                               |
+|                  User Agent List Filter V1 by Saturnx-Dev                     |
 |        _______________________________________________________________        |
 |                                                                               |
-|        Selecciona un navegador para filtrar los User Agents.                  |
+|            Selecciona un navegador para filtrar los User Agents.              |
 |        _______________________________________________________________        |
 |                                                                               |
 |      [1] Chrome                    | Filtrar User Agents de Chrome            |
@@ -64,6 +79,7 @@ def main():
 |                                                                               |
 |_______________________________________________________________________________|
             """)
+            browser_option = input("Selecciona una opción (1, 2, 3, 4): ")
             if browser_option == '1':
                 filtered_agents = filter_by_browser(user_agents, 'Chrome')
             elif browser_option == '2':
@@ -79,13 +95,19 @@ def main():
             for agent in filtered_agents:
                 print(agent)
 
+            # Opción para copiar o guardar los resultados
+            save_option = input("\n¿Deseas copiar los resultados (c) o guardarlos en un archivo (g)? (Presiona Enter para continuar): ")
+            if save_option.lower() == 'g':
+                save_results(filtered_agents)
+
         elif option == '2':
-            os_option = input("""
+            print("""
  _______________________________________________________________________________
 |                                                                               |
-|                             | OPERATING SYSTEM MENU |                         |
+|                            | OPERATING SYSTEM MENU |                          |
 |           _________________________________________________________           |
 |                                                                               |
+|                   User Agent List Filter V1 by Saturnx-Dev                    |
 |        _______________________________________________________________        |
 |                                                                               |
 |        Selecciona un sistema operativo para filtrar los User Agents.          |
@@ -100,6 +122,7 @@ def main():
 |                                                                               |
 |_______________________________________________________________________________|
             """)
+            os_option = input("Selecciona una opción (1, 2, 3, 4, 5, 6): ")
             if os_option == '1':
                 filtered_agents = filter_by_os(user_agents, 'Windows')
             elif os_option == '2':
@@ -118,6 +141,11 @@ def main():
             print("\nUser Agents Filtrados por Sistema Operativo:")
             for agent in filtered_agents:
                 print(agent)
+
+            # Opción para copiar o guardar los resultados
+            save_option = input("\n¿Deseas copiar los resultados (c) o guardarlos en un archivo (g)? (Presiona Enter para continuar): ")
+            if save_option.lower() == 'g':
+                save_results(filtered_agents)
 
         elif option == '3':
             print("Saliendo del programa. ¡Hasta luego!")
